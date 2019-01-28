00edeba7e690040e2fa0d56ab9711707414b3cb7
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2019-01-28 11:54:39 -0800

    Add files via upload

--- techreview2/tech/models.py
+++ techreview2/tech/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#producttype, product, review
+
+class ProductType(models.Model):
+    typename=models.CharField(max_length=255)
+    typedescription=models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.typename
+    
+    class Meta:
+        db_table='producttype'
+
+class Product(models.Model):
+    productname=models.CharField(max_length=255)
+    producttype=models.ForeignKey(ProductType, on_delete=models.DO_NOTHING)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    productentrydate=models.DateField()
+    producturl=models.URLField(null=True, blank=True)
+    productdescription=models.TextField()
+
+    def __str__(self):
+        return self.productname
+    
+    class Meta:
+        db_table='product'
+
+class Review(models.Model):
+    reviewtitle=models.CharField(max_length=255)
+    reviewdate=models.DateField()
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    user=models.ManyToManyField(User)
+    reviewwrating=models.SmallIntegerField()
+    reviewtext=models.TextField()
+
+    def __str__(self):
+        return self.reviewtitle
+    
+    class Meta:
+        db_table='review'
--- techreview2/tech/views.py
+++ techreview2/tech/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ProductType
 
 # Create your views here.
 def index (request):
     return render(request, 'tech/index.html')
+
+def techtypes (request):
+    type_list=ProductType.objects.all()
+    return render (request, 'tech/types.html', {'type_list': type_list})
