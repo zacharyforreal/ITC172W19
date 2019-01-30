6f0ac59124126bcde776c636cbdb936b4c97d030
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2019-01-30 12:04:51 -0800

    Add files via upload

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Resource
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def resource (request):
+    resource_list=Resource.objects.all()
+    return render (request, 'club/resource.html', {'resource_list': resource_list})
