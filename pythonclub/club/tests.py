751687325d4fc06720c5dcf9055d355caf446216
zacharyforreal <36243126+zacharyforreal@users.noreply.github.com>
2019-02-13 11:44:56 -0800

    Update tests.py

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Meeting, MeetingMinutes
+from django.urls import reverse
 
 # Create your tests here.
+class MeetingTest(TestCase):
+    def test_stringOutput(self):
+        meeting=Meeting(meetingtitle='March Meeting')
+        self.assertEqual(str(meeting), meeting.meetingtitle)
+
+    def test_tablename(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+#testing a view
+class TestIndex(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response=self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response=self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'club/index.html')
